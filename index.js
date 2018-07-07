afbb9b600b2e0e318936543abee3d388339655af
qutang <tqshelly@gmail.com>
2018-07-06 20:58:12 -0400

    Add more logging

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ var range = parseFloat(process.argv[3]) || 2;
 
 var WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8848 });
+var port = 8848;
+
+const wss = new WebSocket.Server({ port: port });
 
 wss.on('connection', function(ws) {
     ws.on('message', function(message) {
       console.log('Received: ' + message);
     });
     console.log('connected to client');
-    ws.send('connected');
+    // ws.send('connected');
 });
 
 var broadcast_mbient_data = function(){
@@ -39,6 +41,7 @@ var broadcast_mbient_data = function(){
     
         device.connectAndSetup(function(error) {
             console.log('were connected!');
+            console.log('metawear websocket server at ws://localhost:' + port);
             console.log('Start accelerometer with ' + rate + 'hz ang +-' + range + 'g');
     
             var accelerometer = new device.Accelerometer(device);
@@ -55,9 +58,10 @@ var broadcast_mbient_data = function(){
     
             accelerometer.onChange(function(data) {
                 var current_ts = moment();
-                var current_ts_str = current_ts.format('YYYY-MM-DD HH:mm:ss.SSS')
+                var current_ts_str = current_ts.format('YYYY-MM-DD HH:mm:ss.SSS');
+                var current_ts_ms = current_ts.valueOf() / 1000.0;
                 var data_sample = {
-                    'HEADER_TIME_STAMP': current_ts_str,
+                    'HEADER_TIME_STAMP': current_ts_ms,
                     'X': data.x,
                     'Y': data.y,
                     'Z': data.z
